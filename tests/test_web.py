bfda653f6403857e2bc8d2c734ad7f4c11b95195
slawekradzyminski <slawomir.radzyminski@gmail.com>
2020-01-10 20:39:24 +0100

    pop

---
+++ tests/test_web.py
@@ -0,0 +1,34 @@
+import pytest
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+from selenium.webdriver import Chrome
+
+
+@pytest.fixture
+def browser():
+    # Initialize ChromeDriver
+    driver = Chrome()
+    # Wait implicitly for elements to be ready before attempting interactions
+    driver.implicitly_wait(10)
+
+    # Return the driver object at the end of setup
+    yield driver
+
+    # For cleanup, quit the driver
+    driver.quit()
+
+
+def test_basic_duckduckgo_search(browser):
+    # Set up test case data
+    PHRASE = 'panda'
+
+    # Search for the phrase
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    search_page.search(PHRASE)
+
+    # Verify that results appear
+    result_page = DuckDuckGoResultPage(browser)
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0
+    assert result_page.search_input_value() == PHRASE
